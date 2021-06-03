b82d5d3de07b8413ef3bf8183407e4af0d3af8fc
Adeyemo Tolulope <adeyemotolulope2@gmail.com>
2021-06-03 17:59:03 +0100

    responsive navbar bug fixed

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 let responsive = document.querySelector('.toggle');
 let view = document.querySelector('nav ul');
 let hide = document.querySelector('.content')
-
+let home = document.querySelector('.home')
 responsive.addEventListener("click" , function () {
   if (view.style.display === "none"){
     view.style.display = 'block'
     hide.style.display ='none'
+    home.style.top = '-17em'
     
     
   }
   else{
     view.style.display = 'none',
     hide.style.display ='flex';
-
+    home.style.top = '-5.2em'
+    
   }
 });
 
